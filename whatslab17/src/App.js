a09828f842162d8fdd76546db0165e6007db575d
mdiasp95 <matheus_dias95@hotmail.com>
2021-09-17 19:19:10 -0300

    Entrega projeto

--- whatslab17/src/App.js
+++ whatslab17/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Chat from './Components/Chat';
+import Mensagens from './Components/Mensagens';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>WhatsLab17</div>
       </header>
+      <body className="App-body">
+      <div className="Mensagens-body"> <Mensagens/> </div>
+      <div className="Chat-body"><Chat/> </div>
+      </body>
     </div>
   );
 }
